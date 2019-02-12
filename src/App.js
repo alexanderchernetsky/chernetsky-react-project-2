82a1a11888c4297c584768a2fb59dcda91d097c6
Alexander Chernetsky <alexvnderchernetsky@gmail.com>
2019-02-12 15:17:47 +0300

    Complete cards with description and create pagination

--- src/App.js
+++ src/App.js
@@ -8,30 +8,57 @@ const api_key = 'eeb7c73b7cfc09ed59ca3805d5018bd0';
 
 class App extends Component {
   state = {
+    query: undefined,
     movies: undefined,
     total: undefined,
+    page: undefined,
+    totalPages: undefined,
   };
 
   findMovies = async (e) => {
     e.preventDefault();
     const searchFieldValue = e.target.filmName.value;
     const query = searchFieldValue.replace(' ', '%20');
-    console.log(query);
     const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&page=1&include_adult=false`)
         .then(res => res.json());
 
+    console.log(data);
+
     this.setState({
+      query: query,
       movies: data.results,
       total: data.total_results,
+      page: data.page,
+      totalPages: data.total_pages,
     })
+  };
 
+  switchToPage = async (direction) => {
+    const page = this.state.page + direction;
+    const query = this.state.query;
+    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&page=${page}&include_adult=false`)
+        .then(res => res.json());
+
+    console.log(data);
+
+    this.setState({
+      movies: data.results,
+      page: data.page,
+    })
   };
 
   render() {
+    const {page, totalPages, total} = this.state;
     return (
         <div>
           <Form findMovies={this.findMovies}/>
-          {this.state.total&&<p>Results found: {this.state.total}</p>}
+          {total&&<p>Results found: {total}</p>}
+          {page&&
+          <p>
+            {totalPages&&page !==1&&<span onClick={() => this.switchToPage(-1)} className="badge badge-secondary">previous</span>}
+            Page: {page} from {totalPages}
+            {totalPages&&totalPages !==1&&(page !== totalPages)&&<span onClick={() => this.switchToPage(1)} className="badge badge-secondary">next</span>}
+          </p>}
           {this.state.movies&&<Movies movies={this.state.movies}/>}
         </div>
     );
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,9 +7,12 @@ const Movies = (props) => {
           <div className="card" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="poster" className="card-img-top movie-card"/>
             <div className="card-body">
-              <h5 className="card-title">Card title that wraps to a new line</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+              <h5 className="card-title">{movie.original_title}</h5>
+              <p className="card-title">{movie.release_date}</p>
+              <p className="card-text">
+                {movie.overview.length < 100 ? movie.overview : movie.overview.substr(0, 100)+ '...'}
+              </p>
+              <button className="btn btn-primary">More</button>
             </div>
           </div>
         ))}
