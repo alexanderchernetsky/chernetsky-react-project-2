baf29f3e4e26543f1323b1c7bf288c90dc83410d
Alexander Chernetsky <alexvnderchernetsky@gmail.com>
2019-02-15 14:55:53 +0300

    Create build and correct gitignore file

---
+++ build/precache-manifest.30fb6cc7833e61e959c0a4b431a831fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ec0bd3a20fefe1631fcfdf0c611d1aa",
+    "url": "/static/media/poster3.1ec0bd3a.jpg"
+  },
+  {
+    "revision": "05b740ecc90952e1287f073068bd5195",
+    "url": "/static/media/poster2.05b740ec.jpg"
+  },
+  {
+    "revision": "d1424bdb1f3bd1ff9d3cc28f5b0dd8ae",
+    "url": "/static/media/poster1.d1424bdb.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62b8966a086b4a4ab13b",
+    "url": "/static/js/main.62b8966a.chunk.js"
+  },
+  {
+    "revision": "75e073b47204afbec8d5",
+    "url": "/static/js/1.75e073b4.chunk.js"
+  },
+  {
+    "revision": "62b8966a086b4a4ab13b",
+    "url": "/static/css/main.26994c4e.chunk.css"
+  },
+  {
+    "revision": "58a7aff9bb8bfef2515ea957477e10ca",
+    "url": "/index.html"
+  }
+];
