ab7956ee4f7631a010875a651cd9bc10f5881750
Alexander Chernetsky <a.chernetsky@itransition.com>
2019-05-21 12:50:11 +0300

    Create Popular People and Trending movies sections in HomePage, change slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,27 +1,31 @@
 import React from "react";
 import poster1 from "../img/poster1.jpg";
-import poster2 from "../img/poster2.jpg";
+
 import poster3 from "../img/poster3.jpg";
 
 const Slider = () => {
   return (
-    <div
-      id="carouselExampleSlidesOnly"
-      className="carousel slide"
-      data-ride="carousel"
-      data-interval="4000"
-    >
-      <div className="carousel-inner mx-auto">
-        <div className="carousel-item active ">
-          <img src={poster1} className="d-block w-100" alt="poster1" />
+    <div id="carouselExampleIndicators" className="carousel slide w-100" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active w-100">
+          <img src={poster1} className="d-block w-100" alt="poster1"/>
         </div>
         <div className="carousel-item">
-          <img src={poster2} className="d-block w-100" alt="poster2" />
-        </div>
-        <div className="carousel-item">
-          <img src={poster3} className="d-block w-100" alt="poster3" />
+          <img src={poster3} className="d-block w-100" alt="poster2"/>
         </div>
       </div>
+      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
   );
 };
