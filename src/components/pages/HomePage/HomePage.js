e36ad8f50c53836e98e829e266671c0248eedb5d
Alexander Chernetsky <a.chernetsky@itransition.com>
2019-05-23 11:42:16 +0300

    Create modal window with person info

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -3,15 +3,14 @@ import Header from '../../common/Header/Header';
 import Slider from '../../common/Slider';
 import noPicture from '../../../img/nopicture.gif';
 import './style.sass';
-import * as $ from "jquery";
 
 const api_key = "eeb7c73b7cfc09ed59ca3805d5018bd0";
-
 
 function HomePage(props) {
   const [movies, setMovies] = useState([]);
   const [people, setPeople] = useState([]);
-  const [person, setPerson] = useState([]);
+  const [personInfo, setPersonInfo] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchTrendingMovies = async () => {
@@ -27,29 +26,30 @@ function HomePage(props) {
     const fetchPopularPeople = async () => {
       let response = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=${api_key}&language=en-US&page=1`);
       response = await response.json();
+      console.log(response.results.slice(0, 8));
       setPeople(response.results.slice(0, 8));
     };
 
     fetchPopularPeople();
   }, []);
 
-  const showPersonInfo = personName => async () => {
-    console.log(personName);
-    let personId;
-    await fetch(`https://api.themoviedb.org/3/search/person?api_key=${api_key}&language=en-US&query=${personName}&page=1`)
-      .then(res => res.json())
-      .then(res => {
-        personId = res.results[0].id;
-      });
+  const showPersonInfo = id => async () => {
 
-    await fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=${api_key}&language=en-US`)
+    await fetch(`https://api.themoviedb.org/3/person/${id}?api_key=${api_key}&language=en-US`)
       .then(res => res.json())
       .then(res => {
-        setPerson(res);
+        setPersonInfo(res);
         console.log(res);
       });
 
+    setShowModal(true);
+
   };
+
+  const hideModal = () => {
+    setShowModal(false);
+  };
+
 
   return (
     <div className="container-fluid px-0">
@@ -62,29 +62,14 @@ function HomePage(props) {
           <h5 className="font-weight-bold text-center">Popular people</h5>
           <div className="row flex-wrap d-flex justify-content-center">
             {people && people.map(person => {
-              if (person.profile_path) {
-                return (
-                  <div className="col-6 poster" key={person.id}>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${person.profile_path}`}
-                      alt="poster"
-                      className="card-img-top"
-                    />
-                    <span onClick={showPersonInfo(person.name)} className="poster-caption">{person.name}</span>
-                    <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                      Launch demo modal
-                    </button>
-                  </div>
-                )
-              }
               return (
-                <div className="col-6" key={person.id}>
+                <div className="col-6 poster" key={person.id}>
                   <img
-                    src={noPicture}
+                    src={`https://image.tmdb.org/t/p/w200${person.profile_path ? person.profile_path : noPicture}`}
                     alt="poster"
                     className="card-img-top"
                   />
-                  <span className="poster-caption">{person.name}</span>
+                  <span onClick={showPersonInfo(person.id)} className="poster-caption">{person.name}</span>
                 </div>
               )
             })}
@@ -104,31 +89,30 @@ function HomePage(props) {
                     alt="poster"
                     className="card-img-top"
                   />
-                  <span className="poster-caption">{movie.title}, {movie.vote_average}</span>
+                  <span className="poster-caption">{movie.title}</span>
+                  <span className="badge badge-primary">{movie.vote_average}</span>
                 </div>
               )
             })}
           </div>
         </div>
-        <div className="modal fade" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                ...
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-              </div>
+
+        {
+          showModal &&
+          <>
+            <div className="shadow"></div>
+            <div className="jumbotron modal-window">
+              <h1 className="display-4">{personInfo.name}</h1>
+              <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
+                attention to featured content or information.</p>
+              <hr className="my-4"/>
+              <p>{personInfo.biography}</p>
+              <p className="lead">
+                <button className="btn btn-primary btn-lg" onClick={hideModal} role="button">Close</button>
+              </p>
             </div>
-          </div>
-        </div>
+          </>
+        }
 
       </div>
     </div>
